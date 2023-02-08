5fa923c4ed800e8c17b3ee71b786ce261e5fc7cf
principal kelvo <principalkelvo@gmail.com>
2023-02-08 16:18:49 +0300

    hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import Header from "./components/common/header/Header";
-
+import Home from "./components/home/Home";
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
 }
 
 export default App;
