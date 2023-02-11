7c162c2545058fcee1452c51b0303a77f9ea4b7a
principal kelvo <principalkelvo@gmail.com>
2023-02-11 17:38:38 +0300

    adds logo and link

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 
-
 const Head = () => {
   return (
     <>
       <section className="head">
         <div className="container flexSB">
           <div className="logo">
-            <h1>ACADEMIA</h1>
-            <span>ONLINE EDUCATION & LEARNING</span>
+            <img src="/images/octalideas.svg" alt="octal logo" />
+            {/* <span>ONLINE EDUCATION & LEARNING</span> */}
           </div>
 
           <div className="social">
-            <i className="fab fa-facebook-f icon"></i>
-            <i className="fab fa-instagram icon"></i>
-            <i className="fab fa-twitter icon"></i>
-            <i className="fab fa-youtube icon"></i>
+            <a href="#">
+              <i className="fab fa-facebook-f icon"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-instagram icon"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-twitter icon"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-youtube icon"></i>
+            </a>
           </div>
         </div>
       </section>
