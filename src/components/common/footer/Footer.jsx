56ad66bc4f649d170ec695aad504c1e9c0ce7120
principal kelvo <principalkelvo@gmail.com>
2023-02-14 00:19:57 +0300

    templating the footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="container flexSB">
           <div className="left row">
             <h1>Newsletter - Stay tune and get the latest update</h1>
-            <span>Far far away, behind the word mountains</span>
+            <span>Stay tune and get the latest update</span>
           </div>
           <div className="right row">
             <input type="text" placeholder="Enter email address" />
@@ -20,11 +20,11 @@ const Footer = () => {
       <footer>
         <div className="container padding">
           <div className="box logo">
-            <h1>ACADEMIA</h1>
+            <h1>OCTAL</h1>
             <span>ONLINE EDUCATION & LEARNING</span>
             <p>
-              A small river named Duden flows by their place and supplies it
-              with the necessary regelialia.
+              where creativity meets technology to help you build the life
+              you've always imagined.
             </p>
 
             <i className="fab fa-facebook-f icon"></i>
@@ -73,19 +73,19 @@ const Footer = () => {
             ))}
           </div>
           <div className="box last">
-            <h3>Have a Questions?</h3>
+            <h3>Have a Question?</h3>
             <ul>
               <li>
                 <i className="fa fa-map"></i>
-                203 Fake St. Mountain View, San Francisco, California, USA
+                203 Bega kwa Bega, Baba Dogo, Nairobi, Kenya
               </li>
               <li>
                 <i className="fa fa-phone-alt"></i>
-                +2 392 3929 210
+                +254 799 444 133
               </li>
               <li>
                 <i className="fa fa-paper-plane"></i>
-                info@yourdomain.com
+                octalideas@gmail.com
               </li>
             </ul>
           </div>
@@ -93,8 +93,8 @@ const Footer = () => {
       </footer>
       <div className="legal">
         <p>
-          Copyright ©2022 All rights reserved | This template is made with{" "}
-          <i className="fa fa-heart"></i> by GorkhCoder
+          Copyright ©2023 All rights reserved | This template is made with{" "}
+          <i className="fa fa-heart"></i> by Principal Kelvin
         </p>
       </div>
     </>
