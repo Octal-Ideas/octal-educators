9060456074c5d911d091cf75f317762b2b83a85c
principal kelvo <principalkelvo@gmail.com>
2023-02-15 13:03:41 +0300

    import animation on scroll

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
+import "animate.css/animate.min.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
