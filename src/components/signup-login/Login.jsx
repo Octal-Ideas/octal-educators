549a5af0a97846cf33e864fbfd35cc0637684a5e
Evans <evansroysir@gmail.com>
2023-03-08 10:17:09 +0300

    Add Css Style to Login component

--- src/components/signup-login/Login.jsx
+++ src/components/signup-login/Login.jsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import "./Login.css";
 
 
 function Login() {
@@ -11,7 +12,7 @@ function Login() {
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      
+
     } catch (error) {
       setError(error.message);
     }
@@ -21,18 +22,36 @@ function Login() {
     <div className='cover'>
       <h1>Login</h1>
       <form onSubmit={handleLogin}>
-        <div>
-          <label>Email:</label>
+        <div className='inputBox'>
+          {/* <label>Email:</label> */}
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <span>Email</span>
         </div>
-        <div>
-          <label>Password:</label>
+        <div className='inputBox'>
+          {/* <label>Password:</label> */}
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <span>Password</span>
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" id='btn-login'>Login</button>
         <div>{error}</div>
       </form>
       <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+      <p>Or Log In Using</p>
+
+      <div className="alt-login">
+        <a href="" target="_blank">
+          <i className="fab fa-facebook-f icon"></i>
+        </a>
+        <a href="" target="_blank">
+          <i className="fab fa-google icon"></i>
+        </a>
+        <a href="" target="_blank">
+          <i className="fab fa-twitter icon"></i>
+        </a>
+        <a href="" target="_blank">
+          <i className="fab fa-github icon"></i>
+        </a>
+      </div>
     </div>
   );
 }
