91f08134303cfd090b2286047fc36d4a59260d98
principal kelvo <principalkelvo@gmail.com>
2023-03-13 23:42:32 +0300

    shows details on the console.

--- src/components/signup-login/Register.jsx
+++ src/components/signup-login/Register.jsx
@@ -1,10 +1,47 @@
-import React from "react"; // Importing the React library
+import React, { useState } from "react"; // Importing the React library
 import * as Components from "./Component"; // Importing components from the Components file
 
 function Register() {
   // Creating a function that returns JSX code (React Component)
-  const [signIn, toggle] = React.useState(true); // Creating a state using useState with an initial value of true and array destructuring
+  const [signIn, toggle] = useState(true); // Creating a state using useState with an initial value of true and array destructuring
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
+  // Handles login logic
+  const handleLogin = async (event) => {
+    event.preventDefault(); // Prevents form from submitting
+    console.log("email", email, "password", password);
+    try {
+      // Handle login logic here
+      event.target.reset();
+    } catch (error) {
+      setError(error.message); // Sets error message if an error occurs
+    }
+  };
+
+  const handleRegister = async (event) => {
+    event.preventDefault(); // prevent default form submission behavior
+    console.log("name", name, "email", email, "password", password);
+    try {
+      // TODO: add logic for registering user with backend API
+    } catch (error) {
+      // If an error occurs during registration, set the error state variable
+      setError(error.message);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const input = e.target;
+    if (input.value) {
+      // If the input has a value, add "has-value" class
+      input.classList.add("has-value");
+    } else {
+      // If the input does not have a value, remove "has-value" class
+      input.classList.remove("has-value");
+    }
+  };
   return (
     // Returning the following JSX content
     <>
@@ -15,23 +52,51 @@ function Register() {
           <Components.SignUpContainer signinIn={signIn}>
             {" "}
             {/*  SignUpContainer component with dynamic signIn prop*/}
-            <Components.Form>
+            <Components.Form onSubmit={handleRegister}>
               {" "}
               {/*  Form component*/}
               <Components.Title>Create Account</Components.Title>{" "}
               {/* Title
           component with text as child*/}
-              <Components.Input type="text" placeholder="Name" />{" "}
+              <Components.Input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value); // Update state variable for name
+                  handleInputChange(e);
+                }}
+                required
+              />{" "}
               {/*Input
           component for name field*/}
-              <Components.Input type="email" placeholder="Email" />{" "}
+              <Components.Input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value); // Update state variable for email
+                  handleInputChange(e);
+                }}
+                required
+              />{" "}
               {/*  Input component for email field*/}
-              <Components.Input type="password" placeholder="Password" />{" "}
+              <Components.Input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value); // Update state variable for password
+                  handleInputChange(e);
+                }}
+                required
+              />{" "}
               {/*  Input component for password field*/}
-              <Components.Button>Sign Up</Components.Button>{" "}
+              <Components.Button type="submit">Sign Up</Components.Button>{" "}
               {/* Button component for sign-up*/}
-
-               <Components.Paragraph>Or Register Using</Components.Paragraph>
+              {/* Error message */}
+              <div>{error}</div>
+              <Components.Paragraph>Or Register Using</Components.Paragraph>
               <div className="alt-login">
                 <a href="" target="_blank">
                   <i className="fab fa-facebook-f icon"></i>
@@ -51,7 +116,7 @@ function Register() {
           <Components.SignInContainer signinIn={signIn}>
             {" "}
             {/*  SignInContainer component with dynamic signIn prop*/}
-            <Components.Form>
+            <Components.Form onSubmit={handleLogin}>
               {" "}
               {/*  Form component*/}
               {/*  Title component
@@ -59,9 +124,27 @@ function Register() {
               <Components.Title>Sign in</Components.Title>{" "}
               {/*  Input
           component for email field*/}
-              <Components.Input type="email" placeholder="Email" />{" "}
+              <Components.Input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value); // Update state variable for email
+                  handleInputChange(e);
+                }}
+                required
+              />{" "}
               {/*  Input component for password field*/}
-              <Components.Input type="password" placeholder="Password" />
+              <Components.Input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value); // Update state variable for password
+                  handleInputChange(e);
+                }}
+                required
+              />
               <Components.Anchor href="#">
                 Forgot your password?
               </Components.Anchor>{" "}
@@ -69,6 +152,8 @@ function Register() {
               <Components.Button>Sign In</Components.Button>{" "}
               {/*  Button
           component to sign in*/}
+              {/* Error message */}
+              <div>{error}</div>
               <Components.Paragraph>Or Log In Using</Components.Paragraph>
               <div className="alt-login">
                 <a href="" target="_blank">
